eb7bc272ff9df6191c017f76d75aa565bb8192c9
Minhcoit1 <Minhbuibk86@gmail.com>
2017-09-03 10:30:20 +0700

    login activity

---
+++ app/src/main/java/minhbuibk86/testgithub/LoginActivity.java
@@ -0,0 +1,11 @@
+package minhbuibk86.testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Minhb on 03/09/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+}
